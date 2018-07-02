49554303b5ee0a5b7576574736f142a8053783b8
jiangtt18 <tjiang90111@gmail.com>
2018-07-02 02:23:29 -0700

    made a calculator;

--- calculator.jsx
+++ calculator.jsx
@@ -9,21 +9,58 @@ class Calculator extends React.Component{
       Num2:'',
     };
     this.setNum1 = this.setNum1.bind(this);
-    this.setNum2 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+    this.add = this.add.bind(this);
+    this.substract = this.substract.bind(this);
+    this.multiply = this.multiply.bind(this);
+    this.divide = this.divide.bind(this);
+    this.clear = this.clear.bind(this);
     //your code here
   }
 
   //your code here
   setNum1(e){
-    e.preventdefault();
-    const num1 = e.target.value ? parseInt(e.targe.value) : "";
+    console.log(e.target.value)
+    const num1 = e.target.value ? parseInt(e.target.value) : "";
     this.setState({Num1:num1});
   }
 
   setNum2(e){
-    e.preventdefault();
-    const num2= e.target.value ? parseInt(e.targe.value) : "";
+    const num2= e.target.value ? parseInt(e.target.value) : "";
     this.setState({Num2:num2});
+  }
+
+  add(e){
+    e.preventDefault();
+    let result = this.state.Num1 + this.state.Num2;
+    this.setState({result});
+  }
+
+  substract(e){
+    e.preventDefault();
+    let result = this.state.Num1 - this.state.Num2;
+    this.setState({result});
+  }
+
+  multiply(e){
+    e.preventDefault();
+    let result = this.state.Num1 * this.state.Num2;
+    this.setState({result});
+  }
+
+  divide(e){
+    e.preventDefault();
+    let result = this.state.Num1 / this.state.Num2;
+    this.setState({result});
+  }
+
+  clear(e){
+    e.preventdefault();
+    this.setState({
+      result: 0,
+      Num1:'',
+      Num2:''
+    });
   }
 
   render(){
@@ -33,10 +70,15 @@ class Calculator extends React.Component{
         <input
           onChange = {this.setNum1}
           value = {this.state.Num1}/>
-          <input
+        <input
             onChange = {this.setNum2}
             value = {this.state.Num2}/>
-        //your code will replace this
+          <button onClick = {this.add}>+</button>
+          <button onClick = {this.substract}>-</button>
+          <button onClick = {this.multiply}>*</button>
+          <button onClick = {this.divide}>/</button>
+          <button onClick = {this.clear}>clear</button>
+
       </div>
     );
   }
